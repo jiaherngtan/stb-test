28cbb9450aaa08613af9ec28c73a5a44e3fca922
jiaherngtan <jherngt@gmail.com>
2025-03-06 00:03:39 +0800

    Update WebDriverInit.java

--- src/main/java/services/WebDriverInit.java
+++ src/main/java/services/WebDriverInit.java
@@ -7,14 +7,6 @@ public class WebDriverInit {
 
     protected static WebDriver driver;
 
-    public static WebDriver getDriver() {
-        return driver;
-    }
-
-    public static void setDriver(WebDriver driver) {
-        WebDriverInit.driver = driver;
-    }
-
     public WebDriver initializeDriver() {
         //System.setProperty("webdriver.edge.driver", System.getProperty("user.dir") + "\\webdrivers\\msedgedriver.exe");
         driver = new ChromeDriver();
